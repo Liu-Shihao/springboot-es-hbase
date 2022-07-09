5b4e1a2e9bd79a79ef6ea490f721e3f5309828fc
LiuShihao <liush99@foxmail.com>
2022-07-09 11:16:13 +0800

    new computer

---
+++ springboot-es/src/main/java/com/lsh/dto/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.lsh.dto;
+
+/**
+ * Author: LiuShihao
+ * Date: 2022/7/9 上午9:36
+ * Desc: test MacBook Pro   2022年07月09日09:37:25
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
